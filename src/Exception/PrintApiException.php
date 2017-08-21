bd0021b15c33904c7100ada0bb47a4657fab2d10
Koen Pasman <koen@koenpasman.nl>
2017-08-21 08:30:03 +0200

    Put everything into its own PSR-4 namespace

---
+++ src/Exception/PrintApiException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PrintApi\Exception;
+
+/**
+ * Generic exception thrown by the Print API client.
+ */
+class PrintApiException extends \Exception
+{
+
+}
---
+++ src/Exception/PrintApiResponseException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PrintApi\Exception;
+
+/**
+ * Exception thrown by the Print API client for failed API calls.
+ */
+class PrintApiResponseException extends PrintApiException
+{
+
+}
